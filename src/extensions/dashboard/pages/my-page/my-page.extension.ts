0517e7ad9611f51ab26753369f610e03358f14ca
pharleg <curtis.mcewen@gmail.com>
2026-03-29 21:30:14 -0400

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '611b4f9e-17bb-449c-81be-022914fdbf32',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
